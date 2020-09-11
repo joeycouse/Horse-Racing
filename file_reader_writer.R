784d488ccd3f324eb175b1de0c74dcdf5198a65d
Joey Couse <54423399+joeycouse@users.noreply.github.com>
2020-09-11 11:34:45 -0400

    Reading in all chart files, must fix column types

--- file_reader_writer.R
+++ file_reader_writer.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 library(data.table)
+library(janitor)
 
 horse_files <- 
   Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.ch') %>%
-  map_df(~fread(.))
-
-
-horse_files<-janitor::make_clean_names(horse_files)
-
+  map_df(~fread(.)) %>%
+  clean_names()
 
 # To Do:
 # Make Function that generates additional variables
@@ -18,16 +16,65 @@ chart_files <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.ch
 
 file_path <- "C:/Users/joeyc/Documents/Data Science/Horse Racing/Horse Racing Code/data/ASD20200525.chart"
 
-read_chart_files <- function(file_path){
+read_chart_files <- function(file_paths, return_races=FALSE, return_starters = TRUE){
   
-  header <- read_csv(file_path, n_max =1, col_names = FALSE)
+  all_races = data.frame()
+  all_starters = data.frame()
   
+  for (file_path in file_paths){
+    
+    print(file_path)
+    
+    header <- read_csv(file_path, n_max =1, col_names = FALSE) %>%
+      select(-X1) %>%
+      select(c(1:4)) %>%
+      rename(country = X2, rc_track = X3, rc_date = X4, num_races = X5) %>%
+      mutate(rc_date = as.Date(as.character(rc_date), format = '%Y%m%d'),
+             rc_track = str_trim(str_to_upper(rc_track)))
+      
+    
+    races <- suppressWarnings(read_csv(file_path, col_names = FALSE, skip = 1))%>%
+      filter(X1 == 'R')%>%
+      bind_cols(header, .) %>%
+      rename(
+        record_type = X1, rc_race = X2, breed = X3, race_type = X4, restrictions = X5, sex_restrictions = X6,
+        age_restrictions = X7, division = X8, purse = X9, reverts_money = X10, available_money = X11, paid_to_others = X12, guaranteed_money = X13, added_money = X14,
+        includes_one_money = X16, includes_two_money = X18, includes_three_money = X20, plus_one_money = X22, plus_two_money = X24, 
+        plus_three_money = X26, min_claiming = X27,max_claiming = X28, distance = X30, distance_unit = X31, surface = X32, 
+        num_horses = X34, track_condition = X43, track_variant = X45, speed_number = X46) %>%
+      mutate(restrictions = as.factor(restrictions)) %>%
+      select(-starts_with('X'))
+    
+    all_races <- bind_rows(all_races, races)
+  
+    
+    skip_num <- nrow(races)+1
+    
+    starters <- suppressWarnings(read_csv(file_path, col_names = FALSE, skip = skip_num)) %>%
+      filter(X1 == 'S') %>%
+      bind_cols(header, .) %>%
+      rename(
+        record_type = X1, rc_race = X2, horse_key = X3, horse_name = X4, birthday = X5, breed = X7, sex = X8, weight_carried = X22,
+        horse_weight = X23, medication = X24, equipment = X25, jock_last_name = X27, jock_first_name = X28, trainer_last_name = X31, 
+        trainer_first_name = X32, owner_last_name = X34, owner_first_name = X35, odds = X37, non_betting_starter = X38,
+        favorite = X41, post_position = X42, finish_position = X51) %>%
+      select(-starts_with('X')) %>%
+      mutate(birthday = as.Date(as.character(birthday), format = '%Y%m%d'))
+    
+    all_starters <- bind_rows(all_starters, starters)
+    
+  }
+  
+  if (return_races == TRUE){
+    return(all_races)}
+  
+  else if(return_starters == TRUE){
+      return(all_starters)}
 }
 
-read_csv("C:/Users/joeyc/Documents/Data Science/Horse Racing/Horse Racing Code/data/ASD20200525.chart", n_max =1, col_names = FALSE)
 
-file <- read_file("C:/Users/joeyc/Documents/Data Science/Horse Racing/Horse Racing Code/data/ASD20200525.chart")
-
+###### To Do; Clean up column types; e.g. change strings to factor, or strings to numeric etc.
 
 
-
+races <- read_chart_files(chart_files, return_races = T, return_starters = F)
+starters <- read_chart_files(file_path, return_races = F, return_starters = T)
