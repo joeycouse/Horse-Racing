f9a57dd25e5fcbdda7b6c8286fe1676a42ddd421
Joey Couse <54423399+joeycouse@users.noreply.github.com>
2022-02-16 17:32:38 -0600

    Split chart file reading into separate file. Improve reading speed.

--- file_reader.R
+++ file_reader.R
@@ -1,97 +1,13 @@
 library(tidyverse)
-library(broom)
 library(janitor)
 library(lubridate)
-library(splines2)
-library(ggcorrplot)
 library(feather)
 library(glue)
 
-
-
-
-
-chart_files <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.chart')
 running_line_files <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.chr')
 horse_files <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.ch')
 betting_file_paths <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.pgh')
 special_files <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/*.cs')
-
-read_chart_files <- function(file_paths, return_races=FALSE, return_starters = TRUE){
-  
-  all_races = data.frame()
-  all_starters = data.frame()
-  
-  for (file_path in file_paths){
-  
-    #To Do  
-    #This is pretty ridiculous I have 3 calls to read_csv per Chart File, this needs to be cleaned up quite a bit
-    
-    
-    header <- read_csv(file_path, n_max =1, col_names = FALSE, col_types = 'ccccdcc') %>%
-      select(-X1) %>%
-      select(c(1:4)) %>%
-      rename(country = X2, rc_track = X3, rc_date = X4, num_races = X5) %>%
-      mutate(rc_date = as.Date(as.character(rc_date), format = '%Y%m%d'),
-             rc_track = str_trim(str_to_upper(rc_track)))
-    
-    
-    races <- suppressWarnings(read_csv(file_path, col_names = FALSE, skip = 1, col_types = cols(.default = col_character())))%>%
-      filter(X1 == 'R')%>%
-      bind_cols(header, .) %>%
-      rename(
-        record_type = X1, rc_race = X2, breed = X3, race_type = X4, restrictions = X5, sex_restrictions = X6,
-        age_restrictions = X7, division = X8, purse = X9, reverts_money = X10, available_money = X11, paid_to_others = X12, guaranteed_money = X13, added_money = X14,
-        includes_one_money = X16, includes_two_money = X18, includes_three_money = X20, plus_one_money = X22, plus_two_money = X24, 
-        plus_three_money = X26, min_claiming = X27, max_claiming = X28, abt_dist = X29, rc_distance = X30, distance_unit = X31, surface = X32, course_type = X33,
-        num_horses = X34, track_condition = X43, track_variant = X45, speed_number = X46) %>%
-      mutate(restrictions = as.factor(restrictions), 
-             sex_restrictions = as.factor(sex_restrictions), 
-             distance_unit = as.factor(distance_unit),
-             surface = as.factor(surface),
-             age_restrictions = as.factor(age_restrictions),
-             track_condition = as.factor(track_condition)) %>%
-      select(-starts_with('X')) %>%
-      type_convert() %>%  
-      select(country, rc_track, rc_date, rc_race, race_type, num_horses, purse, rc_distance, distance_unit, surface, track_condition) %>%
-      mutate(distance_unit = if_else(is.na(distance_unit) | distance_unit == 'M' | distance_unit == 'F', 'F', 'Y'),
-             rc_distance = round(rc_distance/100,2)) %>%
-      mutate(rc_distance = if_else(distance_unit == 'Y', round(rc_distance * .454545, 1), rc_distance)) %>%
-      select(-distance_unit, -race_type)
-    
-    all_races <- bind_rows(all_races, races)
-    
-    
-    skip_num <- nrow(races)+1
-    
-    starters <- suppressWarnings(read_csv(file_path, col_names = FALSE, skip = skip_num, col_types = cols(.default = col_character()))) %>%
-      filter(X1 == 'S') %>%
-      bind_cols(header, .) %>%
-      rename(
-        record_type = X1, rc_race = X2, horse_key = X3, horse_name = X4, birthday = X5, breed = X7, sex = X8,
-        medication = X24, equipment = X25, jock_last_name = X27, jock_first_name = X28, trainer_last_name = X31, 
-        trainer_first_name = X32, owner_last_name = X34, owner_first_name = X35, odds = X37, non_betting_starter = X38,
-        favorite = X41, post_position = X42, finish_position = X51, final_len = X63) %>%
-      select(-starts_with('X')) %>%
-      mutate(birthday = as.Date(as.character(birthday), format = '%Y%m%d'),
-             breed = as.factor(breed),
-             sex = as.factor(sex),
-             medication = as.factor(medication),
-             equipment = as.factor(equipment),
-             non_betting_starter = as.factor(non_betting_starter),
-             favorite = as.factor(favorite)) %>%
-      type_convert()
-    
-    all_starters <- bind_rows(all_starters, starters)
-    
-  }
-  
-  if (return_races == TRUE){
-    return(all_races)}
-  
-  else if(return_starters == TRUE){
-    return(all_starters)}
-}
 
 read_running_lines <- function(file_paths){
   
@@ -208,8 +124,6 @@ read_betting_files <- function(file_paths){
 }
 
 running_lines <- read_running_lines(running_line_files)
-races <- read_chart_files(chart_files, return_races = T, return_starters = F)
-starters <- read_chart_files(chart_files, return_races = F, return_starters = T)
 horses <- read_horse_files(horse_files)
 betting <- read_betting_files(betting_file_paths)
 foreign_codes <- read_csv('~/Data Science/Horse Racing/Horse Racing Code/data/foreign_track_codes.csv')
@@ -223,8 +137,6 @@ special <- map_df(special_files, ~read_csv(., trim_ws = TRUE,
   select(-best_track_surface)
 
 write_feather(running_lines, './data/running_lines.feather')
-write_feather(races, './data/races.feather')
-write_feather(starters, './data/starters.feather')
 write_feather(horses, './data/horses.feather')
 write_feather(betting, './data/betting.feather')
 write_feather(special, './data/special.feather')
--- parse_chart_files.R
+++ parse_chart_files.R
@@ -3,92 +3,71 @@ source("chart_file_col_names.R")
 chart_file_paths <- Sys.glob('~/Data Science/Horse Racing/Horse Racing Code/data/chart_files/*.chart')
 
 
-
-
-header <- read_csv(chart_file_paths, n_max = 1, col_names = FALSE, col_types = 'ccccdcc', id = "file_path") |> 
+headers <- read_csv(chart_file_paths, n_max = 1, col_names = FALSE, col_types = 'ccccdcc', id = "file_path") |> 
   magrittr::set_colnames(header_col_names) |> 
   mutate(rc_date = as.Date(as.character(rc_date), format = '%Y%m%d'),
          rc_track = str_trim(str_to_upper(rc_track))) |> 
   select(-record_type)
 
+
 chart_files <- read_csv(chart_file_paths, col_names = FALSE, skip = 1, col_types = cols(.default = col_character()), id = "file_path") |> 
   magrittr::set_colnames(races_col_names) |> 
-  left_join(header, 
+  left_join(headers, 
             by = "file_path") |> 
-  select(-file_path)
+  relocate(file_path, country_code, rc_track, rc_date, rc_race, num_races, day_evening, track_name, .before = everything())
 
 
-#Do this on final frame not each one
-%>%
+races <- 
+  chart_files |> 
+  filter(record_type == "R") |> 
+  select(-record_type) |> 
   mutate(restrictions = as.factor(restrictions), 
          sex_restrictions = as.factor(sex_restrictions), 
          distance_unit = as.factor(distance_unit),
          surface = as.factor(surface),
          age_restrictions = as.factor(age_restrictions),
-         track_condition = as.factor(track_condition)) %>%
-  type_convert() %>%  
-  select(country, rc_track, rc_date, rc_race, race_type, num_horses, purse, rc_distance, distance_unit, surface, track_condition) %>%
+         track_condition = as.factor(track_condition)) |> 
+  type_convert() |> 
   mutate(distance_unit = if_else(is.na(distance_unit) | distance_unit == 'M' | distance_unit == 'F', 'F', 'Y'),
-         rc_distance = round(rc_distance/100,2)) %>%
-  mutate(rc_distance = if_else(distance_unit == 'Y', round(rc_distance * .454545, 1), rc_distance)) %>%
-  select(-distance_unit, -race_type)
+         rc_distance = round(rc_distance/100,2)) |> 
+  mutate(rc_distance = if_else(distance_unit == 'Y', round(rc_distance * .454545, 1), rc_distance)) |> 
+  select(-distance_unit) |> 
+  relocate(file_path, country_code, rc_track, rc_date, rc_race, num_races, day_evening, track_name, .before = everything())
 
 
+total_rows <-
+  chart_files |> 
+  filter(record_type == "S") |> 
+  count(file_path, country_code, rc_track, rc_date) |> 
+  rename(n_max = n)
 
-races <- 
-  chart_file |> 
-  filter(X1 == "R") |> 
-  magrittr::set_colnames(races_col_names) |> 
-  select(-record_type) |> 
-  {\(x) bind_cols(header, x)}()
+starting_row <- 
+  chart_files |> 
+  filter(record_type == "R")|> 
+  count(file_path, country_code, rc_track, rc_date) |> 
+  mutate(skip = n + 1, .keep = "unused") 
 
+starters_index <- 
+  left_join(starting_row, total_rows) |> 
+  select(file_path, skip, n_max)
 
+starters_index <- as.list(starters_index)
 
+starters <- 
+  pmap_dfr(starters_index, 
+                     ~read_csv(..1, col_names = F, skip = ..2, n_max = ..3, show_col_types = F, 
+                               col_types = cols(.default = col_character()), id = "file_path")) |> 
+  magrittr::set_colnames(starter_col_names) |> 
+  left_join(headers, by = "file_path") |> 
+  relocate(file_path, country_code, rc_track, rc_date, rc_race, num_races, day_evening, track_name, .before = everything())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+footnotes <- chart_files |> 
+  filter(record_type == "F") |> 
+  select(file_path, country_code, rc_track, rc_date, rc_race,num_races, day_evening, track_name, footnote_num = breed_type_race, footnote = race_type) |> 
+  group_by(file_path, country_code, rc_track, rc_date, rc_race) |> 
+  summarise(footnote = paste(footnote, collapse = ' '))
   
-
-
-
-
-
-
-%>%
-  filter(X1 == 'R')%>%
-  bind_cols(header, .) %>%
-  rename(
-    record_type = X1, rc_race = X2, breed = X3, race_type = X4, restrictions = X5, sex_restrictions = X6,
-    age_restrictions = X7, division = X8, purse = X9, reverts_money = X10, available_money = X11, paid_to_others = X12, guaranteed_money = X13, added_money = X14,
-    includes_one_money = X16, includes_two_money = X18, includes_three_money = X20, plus_one_money = X22, plus_two_money = X24, 
-    plus_three_money = X26, min_clm = X27, max_clm = X28, abt_dist = X29, rc_distance = X30, distance_unit = X31, surface = X32, course_type = X33,
-    num_horses = X34, track_condition = X43, track_variant = X45, speed_number = X46)
-
-
-
-
-
-
-chart_file |> 
-  filter(X1 == "S") |> 
-  separate(X64,
-           into = col_names,
-           sep = ",") |> 
-  view()
+write_feather(headers, './data/headers.feather')
+write_feather(races, './data/races.feather')
+write_feather(starters, './data/starters.feather')
+write_feather(footnotes, './data/footnotes.feather')
